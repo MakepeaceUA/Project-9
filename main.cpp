4b2144e4d2a1939465ec91a741036bad35f518f1
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-06-09 16:56:05 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+//Задание 1
+//int main()
+//{
+//    setlocale(0, "RUS");
+//    int max = 500;
+//    int number;
+//    int sum = 0;
+
+//    cout << "Введите число:";
+//    cin >> number;
+
+//    while (number <= max)
+//   {
+//        sum += number++;
+//    }
+
+//    cout << "Сумма чисел: " << sum;
+//}
+
+
+//Задание 2
+//int main()
+//{
+//    setlocale(0, "RUS");
+
+//    int first_number;
+//    int second_number;
+//    int result = 1;
+//    int count = 1;
+
+//    cout << "Введите первое число:";
+//    cin >> first_number;
+//    cout << "Введите второе число:";
+//    cin >> second_number;
+
+//    if (second_number != 0)
+//    {
+//        while (count <= second_number)
+//       {
+//            result = result * first_number;
+//           count++;
+//        }
+//   }
+//    cout << result;
+//}
+    
+
+
+//Задание 3
+//int main()
+//{
+//   setlocale(0, "RUS");
+
+//   int number = 1;
+//    int sum = 0;
+  
+
+//   while (number <= 1000) 
+//   {
+//        sum += number++;
+//   }
+//   cout << sum;
+//   cout << "\n";
+//    cout << sum / 1000;
+//}
+
+
+
+
+
+//Задания 4
+//int main()
+//{
+//   setlocale(0, "RUS");
+
+//   double result = 1;
+//   double number;
+//    cout << "Введите цифру:";
+//    cin >> number;
+//    if (number >= 1 && number < 20)
+//    {
+//        while (number <= 20)
+//        {
+//            result *= number++;
+
+//        }
+//        cout << result;
+//    }
+//}
+
+
+
+
+//Задание 5
+//int main()
+//{
+//    setlocale(0, "RUS");
+
+//    double result = 1;
+//    int number;
+//    int num = 1;
+//    cout << "Введите цифру:";
+//    cin >> number;
+
+
+//    while (num <= 10)
+//    {
+//        cout << number << " * " << num << " = " << number * num << "\n";
+//        num++;
+
+//    }
+//}
+      
+    
+    
+    
+    
+    
+    
+    
